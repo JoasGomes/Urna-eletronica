29c63512a8c5a403f5f095e78886921f939497e9
JoasGomes <jojoca.vitor777@gmail.com>
2022-03-23 04:42:56 -0300

    commit urna eletrônica

---
+++ urna eletronica/etapas.js
@@ -0,0 +1,50 @@
+let etapas=[
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de tal',
+                partido: 'abc',
+                fotos:[
+                    {url:'bozo.jpg',legenda:'Vereador',small:false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: ' Beltrano da silva',
+                partido: 'defg',
+                fotos:[
+                    {url:'luladrao.jpg',legenda:'Vereador',small:false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'prefeito',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'ciclano',
+                partido: 'abc',
+                vice:'cic',
+                fotos:[
+                    {url:'',legenda:'Prefeito',small:false},
+                    {url:'',legenda:'Vice-prefeito',small:true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'zulano',
+                partido: 'qwerty',
+                vice:'zul',
+                fotos:[
+                    {url:'',legenda:'Prefeito',small:false},
+                    {url:'',legenda:'Vice-prefeito',small:true}
+                ]
+            }
+        ]
+    }
+]
